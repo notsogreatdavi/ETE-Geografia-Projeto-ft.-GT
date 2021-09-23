2f09f3815881472396923e57d02c59e2cedcac08
notsogreatdavi <ddragmeon3@gmail.com>
2021-09-23 19:04:48 -0300

    Site de geografia atualizado por Marília e Rosendo

---
+++ js/mainAgreste.js
@@ -0,0 +1,6 @@
+/* Tela de loading */
+var i = setInterval(function() {
+    clearInterval(i);
+    document.getElementById("carregando").style.display = "none";
+    document.getElementById("whole").style.display = "block";
+}, 0);
